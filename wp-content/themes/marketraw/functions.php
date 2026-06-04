012cc5336767fc9d1e017b563d17a897f8aded0d
davidodo123 <davidsantiagoybermudez@gmail.com>
2026-06-04 18:07:48 +0200

    Google API integracion

--- wp-content/themes/marketraw/functions.php
+++ wp-content/themes/marketraw/functions.php
@@ -16,29 +16,3 @@ function marketraw_setup(): void {
 }
 add_action( 'after_setup_theme', 'marketraw_setup' );
 
-function marketraw_enqueue(): void {
-    $ver = wp_get_theme()->get( 'Version' );
-
-    wp_enqueue_style(
-        'marketraw-fonts',
-        'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap',
-        [],
-        null
-    );
-
-    wp_enqueue_style(
-        'marketraw-main',
-        get_template_directory_uri() . '/assets/css/main.css',
-        [ 'marketraw-fonts' ],
-        $ver
-    );
-
-    wp_enqueue_script(
-        'marketraw-main',
-        get_template_directory_uri() . '/assets/js/main.js',
-        [],
-        $ver,
-        true
-    );
-}
-add_action( 'wp_enqueue_scripts', 'marketraw_enqueue' );
